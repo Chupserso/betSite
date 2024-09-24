d36a4c509ac98a12cb38aaf9cff10fd7ef783593
chupserso <chipserso@gmail.com>
2024-09-24 12:46:08 +0300

    6 commit

--- src/components/bet/Bet.js
+++ src/components/bet/Bet.js
@@ -86,7 +86,7 @@ export const Bet = (props) => {
                             {finalTennisImg}
                         </div>
                         <div className="score">
-                            <div className="title">Счет матча {score}</div>
+                            <div className="title">{set}: Счет матча {score}</div>
                             <div className="names">{names}</div>
                         </div>
                     </div>
